b72162854743841df09ff1d94bd38bba681abd05
MuhamedAhmed011 <aklmuhamedahmed@gmail.com>
2026-03-10 06:33:26 +0200

    Question01

--- OOPApp3/Program.cs
+++ OOPApp3/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            //a) Composition – The departments depend completely on the university,
+            //so if the university is closed the departments no longer exist.
+
+            //b) Association – The driver only uses the car, and both the driver and the car can exist independently.
+
+            //c) Inheritance – A dog is a type of animal, so Dog inherits from the Animal class.
+
+            //d) Aggregation – A team has players, but the players can still exist even if the team is deleted.
+
+            //e) Dependency – The method temporarily uses the logger as a parameter only inside the method. 
+            #endregion
+
+
         }
     }
 }
